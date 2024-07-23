80ece6508cdd631ee8036474b12abe778eb934c0
Baibhab Shree <2004bshree@gmail.com>
2024-07-23 09:38:07 +0530

    JS

---
+++ JS/date22.js
@@ -0,0 +1,57 @@
+// var c=10;
+// console.log(c);
+
+// let loggedIn = true
+// console.log(typeof(loggedIn))
+
+// let temp;
+// console.log(typeof temp);
+
+// let nameOn= "Cool Name's is a name"
+// console.log(typeof nameOn)
+
+// //non primitive
+// //array[], object{}, function()
+
+// let a=[10,20,30];
+// let b=a;
+// b.pop();
+
+// console.log(b);
+// console.log(a);
+
+// let nameTwo = {
+//     roll: "10",
+//     name: "Newton",
+//     age: "undefined"
+// };
+
+// console.log(nameTwo.roll)
+// console.log(nameTwo.age)
+
+// function functionName(){
+//     console.log("Function is calling");
+// }
+
+// functionName()
+console.log("external JS")
+
+let a=10;
+let b=22;
+let c=a+b;
+let sub=a-b;
+let div=a/b;
+let mul=a*b;
+let mod= a%b;
+console.log(c)
+console.log(sub)
+console.log(div)
+console.log(mul)
+console.log(mod)
+console.log("Sum of "+a+" and "+b+" is "+c)
+console.log("Difference of "+a+" and "+b+" is "+sub)
+console.log("Division of "+a+" and "+b+" is "+div)
+console.log("Product of "+a+" and "+b+" is "+mul)
+console.log("Mod of "+a+" and "+b+" is "+mod)
+
+//(+,-,/,*,%)
---
+++ JS/index.js
@@ -0,0 +1,24 @@
+//console.log(" Hello! I am Baibhab Shree.\n I am currently studying at GIFT Autonomous College.\n I am currently persuing my degree in B.Tech CSE.\n Address: Janla, Bhubaneswar.\n Internship: Web Bocket Pvt. Ltd., Bhubaneswar.")
+
+//variables
+
+let age=20;
+var title="Baibhab"
+var price=2.22;
+var male=true;
+const pi=3.141
+
+//let, const acts like var with some changes
+//Three ways to declare variable: var,let,const
+
+let surname="col"
+console.log(age)
+
+var name="Baibhab"
+
+console.log("My name is "+ name)
+console.log("My age is "+ age)
+
+//alert and prompt in html
+
+prompt("My name is "+name)
