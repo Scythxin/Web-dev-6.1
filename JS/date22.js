0f48067c2bd8255334e4ddfeebbadcd9c5d1885b
Baibhab Shree <2004bshree@gmail.com>
2024-07-24 09:29:05 +0530

    JS

--- JS/date22.js
+++ JS/date22.js
@@ -34,24 +34,60 @@
 // }
 
 // functionName()
-console.log("external JS")
+// console.log("external JS")
 
-let a=10;
-let b=22;
-let c=a+b;
-let sub=a-b;
-let div=a/b;
-let mul=a*b;
-let mod= a%b;
-console.log(c)
-console.log(sub)
-console.log(div)
-console.log(mul)
-console.log(mod)
-console.log("Sum of "+a+" and "+b+" is "+c)
-console.log("Difference of "+a+" and "+b+" is "+sub)
-console.log("Division of "+a+" and "+b+" is "+div)
-console.log("Product of "+a+" and "+b+" is "+mul)
-console.log("Mod of "+a+" and "+b+" is "+mod)
+// let a=10;
+// let b=22;
+// let c=a+b;
+// let sub=a-b;
+// let div=a/b;
+// let mul=a*b;
+// let mod= a%b;
+// console.log(c)
+// console.log(sub)
+// console.log(div)
+// console.log(mul)
+// console.log(mod)
+// console.log("Sum of "+a+" and "+b+" is "+c)
+// console.log("Difference of "+a+" and "+b+" is "+sub)
+// console.log("Division of "+a+" and "+b+" is "+div)
+// console.log("Product of "+a+" and "+b+" is "+mul)
+// console.log("Mod of "+a+" and "+b+" is "+mod)
 
 //(+,-,/,*,%)
+
+//date 23
+
+a = a + 1;
+console.log("After adding 1 by normal method" + a);
+b++;
+console.log("After adding 1 by unary operator" + b);
+
+c--;
+console.log("After subtracting 1 by unary operator" + c);
+
+let n= 100;
+let d = d + 1;
+
+d += 1;
+
+let b = b-1;
+
+b -= 1;
+
+n=120;
+n===120; //Also checks datatype(strictly equal to)
+
+120 != 120;
+
+120 !== 120;
+
+let x = true;
+
+console.log(true && true);
+
+console.log(1<3 && 2<3 && 12>3)
+
+console.log(1>2 || 2<1);
+
+console.log(!(1>3));
